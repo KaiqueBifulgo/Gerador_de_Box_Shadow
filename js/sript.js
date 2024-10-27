ef2c8a7e8d7f470cfec159264eced9eeef7e9fb6
KaiqueBifulgo <kaiquebwork@hotmail.com>
2024-10-27 15:13:22 -0300

    Primeiro commit

---
+++ js/sript.js
@@ -0,0 +1,20 @@
+class boxShadowGenerator {
+
+}
+
+
+// SELEÇÃO DE ELEMENTOS
+const horizontal = document.querySelector("horizontal");
+const horizontalRef = document.querySelector("horizontal-value");
+const vertical = document.querySelector("vertical");
+const verticalRef = document.querySelector("vertical-value");
+const blur = document.querySelector("blur");
+const blurRef = document.querySelector("blur-value");
+const spread = document.querySelector("spread");
+const spreadRef = document.querySelector("spread-value");
+
+const previewBox = document.querySelector("box");
+
+const rule = document.querySelector("rule span");
+const webkitRule = document.querySelector("webkit-rule span");
+const mozRule = document.querySelector("moz-rule span");
